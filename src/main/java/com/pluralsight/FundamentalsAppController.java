59bfaea40b874b36608d60a1d4bb46c0c42611dd
AmitSahooRecipes <amitkumarsahoo84@gmail.com>
2023-03-20 20:51:30 +0530

    Added spring web sample code

--- src/main/java/com/pluralsight/FundamentalsAppController.java
+++ src/main/java/com/pluralsight/FundamentalsAppController.java
@@ -1,7 +1,14 @@
 package com.pluralsight;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class FundamentalsAppController {
-	    String home() {
+	@RequestMapping("/hello")    
+	String home() {
 		return ("Hello Pluralsight!!!!");
 	}
 }
